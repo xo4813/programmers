8ffcc03aa0dfb563c6f66e51255ac1a5c7456012
xo4813 <xo4813@naver.com>
2023-07-13 00:03:31 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/86051. 없는 숫자 더하기/없는 숫자 더하기.js
@@ -0,0 +1,11 @@
+function solution(numbers) {
+    var answer = 0;
+    
+    for (let i = 0; i < 10; i++){
+        if(numbers.indexOf(i) == -1){
+            answer += i  
+        }
+    }
+    
+    return answer;
+}
